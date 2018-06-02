b9ababcffa0eab84c4e7d4dc45e59caca6547f2b
zanr512 <zanr512@gmail.com>
2018-06-02 09:28:51 +0200

    kamera

---
+++ Assets/faceDetect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class faceDetect : MonoBehaviour {
+
+	// Use this for initialization
+    public RawImage tst;
+	void Start () {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        for (int i = 0; i < devices.Length; i++)
+        {
+            print("Webcam available: " + devices[i].name);
+        }
+        WebCamTexture tex = new WebCamTexture(devices[0].name);
+        tst.texture = tex;
+        tex.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
